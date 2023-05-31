3552276ccc474f97bd183c8eb561d7012ec98293
Gabriel-Ponte <gabriel.ponte@isqctag.pt>
2023-05-31 08:10:29 +0100

    Aplicação

--- exportExcell.js
+++ exportExcell.js
@@ -1,8 +1,8 @@
 const XLSX = require('xlsx');
-const Docxtemplater = require('docxtemplater');
+//const Docxtemplater = require('docxtemplater');
 const fs = require('fs');
-const async = require('async');
-const iconv = require('iconv-lite');
+//const async = require('async');
+//const iconv = require('iconv-lite');
 
 // Load the Excel file
 const workbook = XLSX.readFile('Seguimento_ações_2023.xlsx');
@@ -12,11 +12,11 @@ const worksheet = workbook.Sheets[workbook.SheetNames[0]];
 //const jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1, range: 2 });
 // Convert Excel data to JSON, ignoring the first column
 const jsonData = XLSX.utils.sheet_to_json(worksheet, 
-    { header: ['Codigo','Codigo', 'Tema', 'Nome', 'Acao', 'DataInicio', 'DataObjetivo', 'AlertaDias', 'Piloto', 'Notas', 'Finalizado', 'Resultado', 'Links', 'TipoTrabalho', 'OrçamentoAprovado']
-    , range: 2, raw: false });
+    { header: ['Empty','Codigo', 'Tema', 'Nome', 'Acao', 'DataInicio', 'DataObjetivo', 'AlertaDias', 'Piloto', 'Notas', 'Finalizado', 'Resultado', 'Links', 'TipoTrabalho', 'OrçamentoAprovado']
+    , range: 3, raw: false });
 
 //const jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 2, range: 2, raw: false }); // Remove the first two columns (column A and B) from each row
-console.log(jsonData[0].CODIGO)
+console.log(jsonData[0].Codigo)
 // Write JSON data to a file
 fs.writeFile('data.json', JSON.stringify(jsonData, null, 2), (err) => {
   if (err) {
