4dbc370fe7bafcb807c4d146d1a549f646334a75
Gabriel-Ponte <gabriel.ponte@isqctag.pt>
2023-06-01 13:02:14 +0100

    Aplicação

--- client/src/components/VisualizarProjeto.js
+++ client/src/components/VisualizarProjeto.js
@@ -31,6 +31,7 @@ function VisualizarProjeto() {
   const { user } = useSelector((store) => store.utilizador);
   const { projeto , isLoading } = useSelector((store) => store.projeto);
   const [values, setValues] = useState(null);
+  const [getFeriados, setFeriados] = useState([]);
   const [listaDias, setListaDias] = useState([]);
   const [selectedDay, setSelectedDay] = useState();
 
@@ -38,6 +39,11 @@ function VisualizarProjeto() {
 
   useEffect(() => {
     if (projeto) {
+      const today = new Date();
+      const month = selectedDay ? selectedDay.mes : today.getMonth();
+      const year = selectedDay ? selectedDay.ano : today.getFullYear();
+      const dayFer = new Date(year, month, 0);
+      feriadosPortugal(dayFer);
       setValues(createInitialState(projeto.projeto));
       const userLogin = user?.user?.login;
       const projetoId = projeto?.projeto?._id;
@@ -48,30 +54,95 @@ function VisualizarProjeto() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [projeto]);
 
+
+
+  function feriadosPortugal(date) {
+
+    const feriados = [];
+
+    for (let i = date.getFullYear() - 5; i < date.getFullYear() + 5; i++) {
+      feriados.push(
+        { name: "Ano Novo", date: new Date(i, 0, 1) },
+        { name: "Dia da Liberdade", date: new Date(i, 3, 25) },
+        { name: "Dia do Trabalhador", date: new Date(i, 4, 1) },
+        { name: "Dia de Portugal", date: new Date(i, 5, 10) },
+        { name: "Assunção de Nossa Senhora", date: new Date(i, 7, 15) },
+        { name: "Implantação da República", date: new Date(i, 9, 5) },
+        { name: "Dia de Todos os Santos", date: new Date(i, 10, 1) },
+        { name: "Restauração da Independência", date: new Date(i, 11, 1) },
+        { name: "Dia da Imaculada Conceição", date: new Date(i, 11, 8) },
+        { name: "Natal", date: new Date(i, 11, 25) },
+        { name: "Sexta-feira Santa", date: calculateEaster(i, "SextaFeiraSanta") },
+        { name: "Páscoa", date: calculateEaster(i, "DomingoPascoa") },
+        { name: "Segunda-feira de Páscoa", date: calculateEaster(i, "SegundaPascoa") },
+        { name: "Corpo de Deus", date: calculateCorpusChristi(i) }
+      );
+    }
+    setFeriados(feriados);
+    for (const feriado of feriados) {
+      if (
+        date.getDate() === feriado.date.getDate() &&
+        date.getMonth() === feriado.date.getMonth()
+      ) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  function calculateEaster(year, type) {
+    const a = year % 19;
+    const b = Math.floor(year / 100);
+    const c = year % 100;
+    const d = Math.floor(b / 4);
+    const e = b % 4;
+    const f = Math.floor((b + 8) / 25);
+    const g = Math.floor((b - f + 1) / 3);
+    const h = (19 * a + b - d - g + 15) % 30;
+    const i = Math.floor(c / 4);
+    const k = c % 4;
+    const l = (32 + 2 * e + 2 * i - h - k) % 7;
+    const m = Math.floor((a + 11 * h + 22 * l) / 451);
+    const month = Math.floor((h + l - 7 * m + 114) / 31) - 1;
+    const day = ((h + l - 7 * m + 114) % 31) + 1;
+    if (type === "SextaFeiraSanta") {
+      return new Date(year, month, day - 2);
+    } else if (type === "DomingoPascoa") {
+      return new Date(year, month, day);
+    } else if (type === "SegundaPascoa") {
+      return new Date(year, month, day + 1);
+    }
+  }
+
+  function calculateCorpusChristi(ano) {
+    const domingoPascoa = calculateEaster(ano, "DomingoPascoa");
+    return new Date(ano, domingoPascoa.getMonth(), domingoPascoa.getDate() + 60);
+  }
+
   useEffect(() => {
     if (values) {
       const arrayTT = values.TipoTrabalho ? values.TipoTrabalho.split(',') : [];
       const arrayTTH = new Array(arrayTT.length).fill(0);
-
       if (listaDias) {
         let totalHours = 0;
+
         for (let i = 0; i < listaDias.length; i++) {
           if (listaDias[i].tipoDeTrabalhoHoras) {
-            for (let j = 0; j < listaDias[i].tipoDeTrabalhoHoras.length; j++) {
+            for (let j = 0; j < listaDias[i].tipoDeTrabalhoHoras. length; j++) {
               if (listaDias[i].tipoDeTrabalhoHoras[j].projeto === values._id) {
                 const array = listaDias[i].tipoDeTrabalhoHoras[j].horas ? listaDias[i].tipoDeTrabalhoHoras[j].horas.split(',') : [];
+                const values = listaDias[i].tipoDeTrabalhoHoras[j].tipoTrabalho ? listaDias[i].tipoDeTrabalhoHoras[j].tipoTrabalho.split(',') : [];  
                 if (array !== null) {
                   for (let h = 0; h < array.length; h++) {
-                    console.log(j)
                     totalHours += Number(array[h]);
-                    arrayTTH[h] += Number(array[h]);
+                    arrayTTH[values[h]] = (arrayTTH[values[h]] || 0) + Number(array[h]);
                   }
                 }
-                //totalHours += Number(listaDias[i].tipoDeTrabalhoHoras[j].horas);
               }
             }
           }
         }
+
         if (totalHours !== 0) {
           setValues({
             ...values,
@@ -105,13 +176,7 @@ function VisualizarProjeto() {
       return null;
     }
   }
-
-  //const today = new Date();
-  //const diaSelected = selectedDay ? selectedDay.dia : 0;
-  //const month = selectedDay ? selectedDay.mes : today.getMonth();
-  //const year = selectedDay ? selectedDay.ano : today.getFullYear();
-  console.log(values.Piloto)
-  console.log(projeto.projeto.Piloto)
+  console.log(selectedDay)
   return (
     <Wrapper>
       <div className="mainVisualiza">
@@ -211,6 +276,7 @@ function VisualizarProjeto() {
                 <div className="col-12 mb-5 text-center">
                   <Calendar
                     handleChange={handleChangeCalendario}
+                    feriados={getFeriados}
                     inserted={listaDias}
                     inicio={values?.DataInicio}
                     objetivo={values?.DataObjetivo}
@@ -222,7 +288,7 @@ function VisualizarProjeto() {
                     <h5>Data Inicio</h5>
                   </div>
                   <div className="col-3">
-                    <p>{values.DataInicio ? new Date(values.DataInicio).toLocaleDateString('en-CA') : ''}</p>
+                    <p>{values.DataInicio ? new Date(values.DataInicio).toLocaleDateString('en-CA') : 'Sem data Inicial'}</p>
                   </div>
 
                   {values.Finalizado === true ? (
@@ -240,7 +306,7 @@ function VisualizarProjeto() {
                         <h5>Data Objetivo</h5>
                       </div>
                       <div className="col-3">
-                        <p>{values.DataObjetivo ? new Date(values.DataObjetivo).toLocaleDateString('en-CA') : ''}</p>
+                        <p>{values.DataObjetivo ? new Date(values.DataObjetivo).toLocaleDateString('en-CA') : 'Sem data Objetivo'}</p>
                       </div>
                     </>
                   )}
@@ -260,14 +326,18 @@ function VisualizarProjeto() {
                   </div>
                   {values.TipoTrabalho && values.TipoTrabalho.length > 0 ? (
                     values.TipoTrabalho.split(",").map((t, i) => (
+                      
+                      values.NumeroHorasTipoTrabalho[t] && values.NumeroHorasTipoTrabalho[t]> 0 ? (
                       <div className="row mb-3" key={i}>
                         <div className="col-6">
                           <p>{t}</p>
                         </div>
                         <div className="col-6">
-                          <p>{values.NumeroHorasTipoTrabalho[i]}</p>
+                          <p>{values.NumeroHorasTipoTrabalho[t]}</p>
                         </div>
-                      </div>
+                      </div>                      
+                      ):
+                        null
                     ))
                   ) : (
                     <div>
